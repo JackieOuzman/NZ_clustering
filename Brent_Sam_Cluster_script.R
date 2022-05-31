ceaa162be05ff88a382b7b512d196582a500aff1
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-05-31 14:10:22 +0930

    some stuff takes too long to run and I have hashed it out the AIC is quick tho

--- Brent_Sam_Cluster_script.R
+++ Brent_Sam_Cluster_script.R
@@ -133,12 +133,13 @@ plot_grid(p1, p2, p3, p4, p5, p6, labels = c("k2", "k3", "k4", "k5", "k6", "k7")
 
 set.seed(31)
 
-fviz_nbclust(qual_scaled, kmeans, method = "wss", k.max = 24) + theme_minimal() + ggtitle("elbow")
-
-gap_stat<-clusGap(qual_scaled, FUN = kmeans, nstart = 30, K.max = 24, B = 50)
-fviz_gap_stat(gap_stat) + theme_minimal() + ggtitle("fviz_gap_stat: gap statistic")
-
-fviz_nbclust(qual_scaled, kmeans, method = "silhouette", k.max = 24) + theme_minimal() + ggtitle("Silhouette")
+#I think this takes a long time
+# fviz_nbclust(qual_scaled, kmeans, method = "wss", k.max = 24) + theme_minimal() + ggtitle("elbow")
+# 
+# gap_stat<-clusGap(qual_scaled, FUN = kmeans, nstart = 30, K.max = 24, B = 50)
+# fviz_gap_stat(gap_stat) + theme_minimal() + ggtitle("fviz_gap_stat: gap statistic")
+# 
+# fviz_nbclust(qual_scaled, kmeans, method = "silhouette", k.max = 24) + theme_minimal() + ggtitle("Silhouette")
 
 ssc<-data.frame(
   kmeans = c(2,3,4,5,6,7,8),
@@ -154,26 +155,27 @@ ssc %>% ggplot(., aes(x=kmeans, y=log10(value), fill = measurement)) +
   xlab("Number of Clusters") + ylab("Log10 Total Sum of Squares") +
   scale_x_discrete(name = "Number of Clusters", limits = c("0", "2", "3", "4", "5", "6", "7", "8"))
 
-res.nbclust<-NbClust(qual_scaled, distance = "euclidean",
-                     min.nc = 2, max.nc = 9,
-                     method = "complete", index = "all")
-
-factoextra::fviz_nbclust(res.nbclust) + theme_minimal() + ggtitle("NbClust's optimal number of clusters")
-
-
-
-
-ss_kmeans<- t(sapply(2:14,
-                     FUN = function(k)
-                       kmeans(x = qual_scaled,
-                              centers = k,
-                              nstart = 20,
-                              iter.max = 25)[c('tot.withinss', 'betweenss')]))
-plot(2:14, unlist(ss_kmeans[,1]), xlab = 'Clusters', ylab = 'Within Cluster SSE')
-
-tot.ss <- sum(apply(qual_scaled, 2, var)) * (nrow(qual_scaled) - 1)
-var_explained<- unlist(ss_kmeans[,2]) / tot.ss
-plot(2:14, var_explained, xlab = 'Clusters', ylab = '% of Total Variation Explained')
+#not run
+# res.nbclust<-NbClust(qual_scaled, distance = "euclidean",
+#                      min.nc = 2, max.nc = 9,
+#                      method = "complete", index = "all")
+# 
+# factoextra::fviz_nbclust(res.nbclust) + theme_minimal() + ggtitle("NbClust's optimal number of clusters")
+# 
+# 
+# 
+# 
+# ss_kmeans<- t(sapply(2:14,
+#                      FUN = function(k)
+#                        kmeans(x = qual_scaled,
+#                               centers = k,
+#                               nstart = 20,
+#                               iter.max = 25)[c('tot.withinss', 'betweenss')]))
+# plot(2:14, unlist(ss_kmeans[,1]), xlab = 'Clusters', ylab = 'Within Cluster SSE')
+# 
+# tot.ss <- sum(apply(qual_scaled, 2, var)) * (nrow(qual_scaled) - 1)
+# var_explained<- unlist(ss_kmeans[,2]) / tot.ss
+# plot(2:14, var_explained, xlab = 'Clusters', ylab = '% of Total Variation Explained')
 
 kmeansAIC<- function(fit){
   m = ncol(fit$centers)
@@ -188,34 +190,50 @@ aic_k<- sapply(2:14, FUN =
 plot(2:14, aic_k, xlab = 'Clusters', ylab = 'AIC from kmeans')
 
 
-kmeansBIC <- function(fit){
-  m = ncol(fit$centers) 
-  n = length(fit$cluster)
-  k = nrow(fit$centers)
-  D = fit$tot.withinss
-  return(D + log(n) * m * k) 
-}
+# kmeansBIC <- function(fit){
+#   m = ncol(fit$centers) 
+#   n = length(fit$cluster)
+#   k = nrow(fit$centers)
+#   D = fit$tot.withinss
+#   return(D + log(n) * m * k) 
+# }
+# 
+# bic_k <- sapply(2:14, FUN = 
+#                   function(k) 
+#                     kmeansBIC(kmeans(qual_scaled, centers = k, nstart = 20, iter.max = 25)))
+# 
+# plot(2:14, aic_k, xlab = 'Clusters', ylab = 'BIC from kmeans')
 
-bic_k <- sapply(2:14, FUN = 
-                  function(k) 
-                    kmeansBIC(kmeans(qual_scaled, centers = k, nstart = 20, iter.max = 25)))
-
-plot(2:14, aic_k, xlab = 'Clusters', ylab = 'BIC from kmeans')
-
-kmeans2 <- function(data, center_range, iter.max, nstart, plot = TRUE){
-  
-  #fit kmeans for each center
-  all_kmeans <- lapply(center_range, 
-                       FUN = function(k) 
-                         kmeans(data, center = k, iter.max = iter.max, nstart = nstart))
-  
-  #extract AIC from each
-  all_aic <- sapply(all_kmeans, kmeansAIC)
-  #extract BIC from each
-  all_bic <- sapply(all_kmeans, kmeansBIC)
-  #extract tot.withinss
-  all_wss <- sapply(all_kmeans, FUN = function(fit) fit$tot.withinss)
-  #extract between ss
-  btwn_ss <- sapply(all_kmeans, FUN = function(fit) fit$betweenss)
-  #extract totall sum of squares
-  tot_ss <- all_kmeans[[1]]$totss
+# kmeans2 <- function(data, center_range, iter.max, nstart, plot = TRUE){
+#   
+#   #fit kmeans for each center
+#   all_kmeans <- lapply(center_range, 
+#                        FUN = function(k) 
+#                          kmeans(data, center = k, iter.max = iter.max, nstart = nstart))
+#   
+#   #extract AIC from each
+#   all_aic <- sapply(all_kmeans, kmeansAIC)
+#   #extract BIC from each
+#   all_bic <- sapply(all_kmeans, kmeansBIC)
+#   #extract tot.withinss
+#   all_wss <- sapply(all_kmeans, FUN = function(fit) fit$tot.withinss)
+#   #extract between ss
+#   btwn_ss <- sapply(all_kmeans, FUN = function(fit) fit$betweenss)
+#   #extract totall sum of squares
+#   tot_ss <- all_kmeans[[1]]$totss
+#   #plot or no plot?
+#   if(plot){
+#     par(mfrow = c(2,2))
+#     with(clust_res,{
+#       plot(Clusters, AIC)
+#       plot(Clusters, BIC)
+#       plot(Clusters, WSS, ylab = 'Within Cluster SSE')
+#       plot(Clusters, BSS / TSS, ylab = 'Prop of Var. Explained')
+#     })
+#   }else{
+#     return(clust_res)
+#   }
+#   
+# }
+# 
+# kmeans2(data = qual_scaled, center_range = 2:15, iter.max = 20, nstart = 25)
